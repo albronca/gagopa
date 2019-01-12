d01cc3356ba2916787e8571dc15688277c988d82
Alexander Bronca <alexander.bronca@gmail.com>
2019-01-12 01:34:33 -0500

    Add language toggle

--- src/Main.elm
+++ src/Main.elm
@@ -9,6 +9,8 @@ import Element.Input as Input
 import Html exposing (Html)
 import Http
 import Json.Decode exposing (..)
+import String.Extra exposing (ellipsis)
+import Url.Builder
 
 
 main : Program () Model Msg
@@ -27,8 +29,15 @@ main =
 
 type alias Model =
     { query : String
+    , language : Language
+    , showResults : Bool
     , results : List SongData
     }
+
+
+type Language
+    = English
+    | Spanish
 
 
 type alias SongData =
@@ -38,9 +47,21 @@ type alias SongData =
     }
 
 
+languageToString : Language -> String
+languageToString language =
+    case language of
+        English ->
+            "English"
+
+        Spanish ->
+            "Spanish"
+
+
 initialModel : Model
 initialModel =
     { query = ""
+    , language = English
+    , showResults = False
     , results = []
     }
 
@@ -55,27 +76,33 @@ init _ =
 
 
 type Msg
-    = QueryChange String
+    = ApiResponse (Result Http.Error (List SongData))
+    | QueryChange String
+    | SelectLanguage Language
     | StartSearch
-    | ApiResponse (Result Http.Error (List SongData))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        QueryChange newQuery ->
-            ( { model | query = newQuery }, Cmd.none )
-
-        StartSearch ->
-            ( model, getJson model.query )
-
         ApiResponse response ->
             case response of
                 Ok newResults ->
-                    ( { model | results = newResults }, Cmd.none )
+                    ( { model | showResults = True, results = newResults }
+                    , Cmd.none
+                    )
 
                 Err _ ->
                     ( model, Cmd.none )
+
+        QueryChange newQuery ->
+            ( { model | query = newQuery }, Cmd.none )
+
+        SelectLanguage newLanguage ->
+            ( { model | language = newLanguage }, Cmd.none )
+
+        StartSearch ->
+            ( model, getJson model )
 
 
 
@@ -112,8 +139,12 @@ view model =
             , spacing 40
             ]
             [ header
-            , searchBox model.query
-            , resultsList model.results
+            , searchBox model
+            , if model.showResults then
+                resultsList model.results
+
+              else
+                none
             ]
 
 
@@ -144,40 +175,110 @@ header =
             ]
 
 
-searchBox : String -> Element Msg
-searchBox inputText =
-    row [ centerX, width <| maximum 600 <| fill ]
-        [ Input.text
-            [ width <| fillPortion 3
-            , Background.color black
-            , Border.color pink
-            , Border.roundEach
-                { topLeft = 5
-                , topRight = 0
-                , bottomLeft = 5
-                , bottomRight = 0
+languageSelect : Language -> Element Msg
+languageSelect language =
+    Input.radioRow [ Font.size 16 ]
+        { onChange = SelectLanguage
+        , options = languageOptions
+        , selected = Just language
+        , label = Input.labelHidden "Language"
+        }
+
+
+languageOptions : List (Input.Option Language Msg)
+languageOptions =
+    [ languageOption
+        [ padding 10
+        , Border.widthEach
+            { top = 1
+            , right = 1
+            , bottom = 0
+            , left = 1
+            }
+        , Border.roundEach
+            { topLeft = 5
+            , topRight = 0
+            , bottomLeft = 0
+            , bottomRight = 0
+            }
+        ]
+        English
+    , languageOption
+        [ padding 10
+        , Border.widthEach
+            { top = 1
+            , right = 1
+            , bottom = 0
+            , left = 0
+            }
+        , Border.roundEach
+            { topLeft = 0
+            , topRight = 5
+            , bottomLeft = 0
+            , bottomRight = 0
+            }
+        ]
+        Spanish
+    ]
+
+
+languageOption : List (Attribute Msg) -> Language -> Input.Option Language Msg
+languageOption attributes language =
+    Input.optionWith language
+        (\optionState ->
+            case optionState of
+                Input.Idle ->
+                    el attributes <| text <| languageToString language
+
+                Input.Focused ->
+                    el (attributes ++ [ Background.color purple ]) <|
+                        text <|
+                            languageToString language
+
+                Input.Selected ->
+                    el (attributes ++ [ Background.color purple ]) <|
+                        text <|
+                            languageToString language
+        )
+
+
+searchBox : Model -> Element Msg
+searchBox model =
+    column [ width fill ]
+        [ languageSelect model.language
+        , row [ centerX, width <| maximum 600 <| fill ]
+            [ Input.text
+                [ width <| fillPortion 3
+                , Background.color black
+                , Border.color pink
+                , Border.roundEach
+                    { topLeft = 0
+                    , topRight = 0
+                    , bottomLeft = 5
+                    , bottomRight = 0
+                    }
+                ]
+                { onChange = QueryChange
+                , text = model.query
+                , placeholder = searchInputPlaceholder
+                , label = Input.labelHidden "Search"
+                }
+            , Input.button
+                [ Font.center
+                , width <| fillPortion 1
+                , height fill
+                , Border.width 1
+                , Border.roundEach
+                    { topLeft = 0
+                    , topRight = 5
+                    , bottomLeft = 0
+                    , bottomRight = 5
+                    }
+                ]
+                { onPress = Just StartSearch
+                , label = text "Search"
                 }
             ]
-            { onChange = QueryChange
-            , text = inputText
-            , placeholder = searchInputPlaceholder
-            , label = Input.labelHidden "Search"
-            }
-        , Input.button
-            [ Font.center
-            , width <| fillPortion 1
-            , height fill
-            , Border.width 1
-            , Border.roundEach
-                { topLeft = 0
-                , topRight = 5
-                , bottomLeft = 0
-                , bottomRight = 5
-                }
-            ]
-            { onPress = Just StartSearch
-            , label = text "Search"
-            }
         ]
 
 
@@ -190,29 +291,37 @@ searchInputPlaceholder =
 
 resultsList : List SongData -> Element Msg
 resultsList results =
-    Element.table
-        [ width fill
-        , Border.width 1
-        , Border.rounded 5
-        , height fill
-        , height shrink
-        ]
-        { data = results
-        , columns =
-            [ { header = resultsListHeader "Song Title"
-              , width = fill
-              , view = .title >> resultsListCell [ Font.alignLeft ]
-              }
-            , { header = resultsListHeader "Artist Name"
-              , width = fill
-              , view = .artist >> resultsListCell [ Border.widthXY 1 0, Border.dotted ]
-              }
-            , { header = resultsListHeader "Code"
-              , width = fill
-              , view = .code >> resultsListCell []
-              }
+    if List.isEmpty results then
+        el [ centerX ] <| text "No results found"
+
+    else
+        column [ width fill ]
+            [ Element.table
+                [ width fill
+                , Border.width 1
+                , Border.rounded 5
+                , clipY
+                , scrollbarY
+                , height <| maximum 350 <| fill
+                ]
+                { data = results
+                , columns =
+                    [ { header = resultsListHeader "Song Title"
+                      , width = fill
+                      , view = .title >> resultsListCell [ Font.alignLeft ]
+                      }
+                    , { header = resultsListHeader "Artist Name"
+                      , width = fill
+                      , view = .artist >> resultsListCell [ Border.widthXY 1 0, Border.dotted ]
+                      }
+                    , { header = resultsListHeader "Code"
+                      , width = fill
+                      , view = .code >> resultsListCell []
+                      }
+                    ]
+                }
+            , el [ centerX ] <| text "1|2|3"
             ]
-        }
 
 
 resultsListHeader : String -> Element Msg
@@ -233,8 +342,9 @@ resultsListHeader headerText =
 
 resultsListCell : List (Attribute Msg) -> String -> Element Msg
 resultsListCell attributes cellText =
-    text cellText
-        |> el ([ padding 10, Font.center ] ++ attributes)
+    ellipsis 23 cellText
+        |> text
+        |> el ([ padding 10, Font.center, Font.size 16 ] ++ attributes)
 
 
 
@@ -270,11 +380,15 @@ pink =
 -- API
 
 
-getJson : String -> Cmd Msg
-getJson query =
+getJson : Model -> Cmd Msg
+getJson model =
     let
         url =
-            "http://gagopa.herokuapp.com/songs?query=" ++ query
+            Url.Builder.crossOrigin "https://gagopa.herokuapp.com"
+                [ "songs" ]
+                [ Url.Builder.string "query" model.query
+                , Url.Builder.string "language" (languageToString model.language)
+                ]
     in
     Http.send ApiResponse <|
         Http.get url songDataListDecoder
