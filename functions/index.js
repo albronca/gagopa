f77f58d0081968e2108e51cf38c654b2253d2340
Alexander Bronca <alexander.bronca@tapad.com>
2019-07-16 16:05:25 -0400

    Add firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,52 @@
+const functions = require('firebase-functions');
+const request = require('request');
+const xmlParser = require('xml2js');
+
+exports.songs = functions.https.onRequest((req, res) => {
+  if (!req.query.search || req.query.search.length === 0) {
+    res.status(200).send([]);
+  } else {
+    const url = functions.config().songlist.url;
+    const queryString = buildQueryString(req.query);
+    request(`${url}?${queryString}`, (error, songListRes, body) => {
+      if (!error && songListRes.statusCode === 200) {
+        parseSongListResponse(body, (result) => {
+          res.status(200).send(result);
+        });
+      } else {
+        res.status(500).send(`Error: ${error}, Status Code: ${songListRes.statusCode}`);
+      }
+    });
+  }
+});
+
+function parseSongListResponse(xml, cb) {
+  xmlParser.parseString(xml, (error, result) => {
+      const songs = result.xml.songs[0].song.map((song) => ({
+        code: song.number[0],
+        title: song.title[0],
+        artist: song.artist[0],
+      }));
+      cb(songs);
+  });
+}
+
+function buildQueryString({ search, language }) {
+  const params = {
+    api: functions.config().songlist.apiname,
+    method: 'searchSongs',
+    by: 'both',
+    per_page: 100,
+    language,
+    page: 1,
+    search: formattedSearch(search)
+  };
+
+  return Object.keys(params).reduce((queryString, paramName) => {
+    return queryString.concat(`${paramName}=${params[paramName]}&`)
+  }, '');
+}
+
+function formattedSearch(search) {
+  return search.replace(/\s/gi, '+');
+}
